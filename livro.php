41daed56245cf9c40705c62d8ec071e38c468768
Miguel-Mernick <mernickmiguel@gmail.com>
2025-04-14 16:03:35 -0300

    .

--- livro.php
+++ livro.php
@@ -7,7 +7,7 @@ class Livro {
         $this->livros = [
             ["titulo" => "O Espião que Saiu do Frio", "autor" => "John le Carré", "ano" => 1963],
             ["titulo" => "Caçada ao Outubro Vermelho", "autor" => "Tom Clancy", "ano" => 1984],
-            ["titulo" => "A Volta dos que não Foram", "autor" => "Machado de Assis", "ano" => 1839],
+            ["titulo" => "A Volta dos que não Foram", "autor" => "Machado de Assis", "ano" => 1952],
             ["titulo" => "Operação Valkiria", "autor" => "Pierre Galante", "ano" => 1967],
             ["titulo" => "Agente Secreto", "autor" => "Joseph Conrad", "ano" => 1907],
         ];
